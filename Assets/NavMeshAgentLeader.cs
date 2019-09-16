34acc227de1dbc23c3c3e15e1e3d3d0a6f4d5e6d
Gregory-Baker <gregory.baker@brl.ac.uk>
2019-09-16 14:25:52 +0100

    Scripts for follow the leader

---
+++ Assets/NavMeshAgentLeader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class NavMeshAgentLeader : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+    NavMeshSurface surface;
+
+    public float distanceAhead = 0.2f;
+
+    public SteamVR_Action_Boolean confirmTargetAction;
+    public Hand hand;
+
+    private void OnEnable()
+    {
+        if (hand == null)
+            hand = this.GetComponent<Hand>();
+
+        if (confirmTargetAction == null)
+        {
+            Debug.LogError("<b>[SteamVR Interaction]</b> No action assigned");
+            return;
+        }
+
+        confirmTargetAction.AddOnChangeListener(OnConfirmActionChange, hand.handType);
+    }
+
+    private void OnDisable()
+    {
+        if (confirmTargetAction != null)
+        {
+            confirmTargetAction.RemoveOnChangeListener(OnConfirmActionChange, hand.handType);
+        }
+    }
+
+    private void OnConfirmActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue)
+    {
+        if (newValue)
+        {
+            //MoveTowardsFirstPoint(distanceAhead);
+        }
+    }
+
+    public IEnumerator MoveTowardsFirstPoint(float distance)
+    {
+        yield return new WaitForSeconds(0.05f);
+        var vector = agent.path.corners[1] - transform.position;
+        var distanceMin = Mathf.Min(distance, vector.magnitude);
+        print(distanceMin);
+        transform.Translate(vector * distanceMin, Space.World);
+    }
+
+    void Awake()
+    {
+        if (surface != null)
+        {
+            surface.BuildNavMesh();
+        }
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Assets/NavMeshFollower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavMeshFollower : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed;
+    public float angularSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        targetDir.y = 0;
+
+        if (targetDir.magnitude > 0.05)
+        {
+            // The step size is equal to speed times frame time.
+            float step = Mathf.Deg2Rad * angularSpeed * Time.deltaTime;
+
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+
+            // Move our position a step closer to the target.
+            transform.rotation = Quaternion.LookRotation(newDir);
+
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
